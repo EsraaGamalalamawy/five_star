e9a28b58e799de7d973af4ba84d47748e83b605a
EsraaGamalalamawy <esraagamal@gmail.com>
2023-09-19 04:11:18 +0300

    five_star

---
+++ js/tem2.js
@@ -0,0 +1,99 @@
+var li1=document.getElementById("li1");
+var c1=document.getElementById("c1")
+var t1=document.getElementById("t1")
+var img1=document.getElementById("boxim1")
+var li2=document.getElementById("li2");
+var c2=document.getElementById("c2")
+var t2=document.getElementById("t2")
+var img2=document.getElementById("boxim2")
+var li3=document.getElementById("li3");
+var c3=document.getElementById("c3")
+var t3=document.getElementById("t3")
+var img3=document.getElementById("boxim3")
+var li4=document.getElementById("li4");
+var c4=document.getElementById("c4")
+var t4=document.getElementById("t4")
+var img4=document.getElementById("boxim4")
+var li5=document.getElementById("li5");
+var c5=document.getElementById("c5")
+var t5=document.getElementById("t5")
+var img5=document.getElementById("boxim5")
+var li6=document.getElementById("li6");
+var c6=document.getElementById("c6")
+var t6=document.getElementById("t6")
+var img6=document.getElementById("boxim6")
+var x1=document.getElementById("x1")
+var x2=document.getElementById("x2")
+var x3=document.getElementById("x3")
+var x4=document.getElementById("x4")
+var x5=document.getElementById("x5")
+var x6=document.getElementById("x6")
+
+li1.onclick=()=>{
+    c1.style.visibility="visible";
+    c1.style.width="90vw"
+    c1.style.minHeight="70vh"
+    c1.style.transition="1s"
+    t1.style.opacity="1"
+    img1.style.opacity="1"
+}
+x1.onclick=()=>{
+    c1.style.visibility="hidden";
+}
+li2.onclick=()=>{
+    c2.style.visibility="visible";
+    c2.style.width="90vw"
+    c2.style.minHeight="70vh"
+    c2.style.transition="1s"
+    t2.style.opacity="1"
+    img2.style.opacity="1"
+}
+x2.onclick=()=>{
+    c2.style.visibility="hidden";
+}
+li3.onclick=()=>{
+    c3.style.visibility="visible";
+    c3.style.width="90vw"
+    c3.style.minHeight="70vh"
+    c3.style.transition="1s"
+    t3.style.opacity="1"
+    img3.style.opacity="1"
+}
+x3.onclick=()=>{
+    c3.style.visibility="hidden";
+}
+li4.onclick=()=>{
+    c4.style.visibility="visible";
+    c4.style.width="90vw"
+    c4.style.minHeight="70vh"
+    c4.style.transition="1s"
+    t4.style.opacity="1"
+    img4.style.opacity="1"
+}
+x4.onclick=()=>{
+    c4.style.visibility="hidden";
+}
+li5.onclick=()=>{
+    c5.style.visibility="visible";
+    c5.style.width="90vw"
+    c5.style.minHeight="70vh"
+    c5.style.transition="1s"
+    t5.style.opacity="1"
+    img5.style.opacity="1"
+}
+x5.onclick=()=>{
+    c5.style.visibility="hidden";
+}
+li6.onclick=()=>{
+    c6.style.visibility="visible";
+    c6.style.width="90vw"
+    c6.style.minHeight="70vh"
+    c6.style.transition="1s"
+    t6.style.opacity="1"
+    img6.style.opacity="1"
+}
+x6.onclick=()=>{
+    c6.style.visibility="hidden";
+}
+
+
